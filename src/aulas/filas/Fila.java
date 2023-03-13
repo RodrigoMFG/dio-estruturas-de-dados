a619c16aa45aef5e086dc012f0ecaf14228ddd82
Rodrigo <rodrigo.marquesg@gmail.com>
2023-03-13 20:32:28 -0300

    Refatoracao da fila para implementar a fila utilizando generics

--- src/aulas/filas/Fila.java
+++ src/aulas/filas/Fila.java
@@ -1,19 +1,20 @@
 package aulas.filas;
 
-public class Fila {
+public class Fila<T> {
 
-    private No refNoEntradaFila;
+    private No<T> refNoEntradaFila;
 
     public Fila(){
         this.refNoEntradaFila = null;
     }
 
-    public void enqueue(No novoNo){
+    public void enqueue(T object){
+        No novoNo = new No(object);
         novoNo.setRefNo(refNoEntradaFila);
         refNoEntradaFila = novoNo;
     }
 
-    public No first(){
+    public T first(){
         if(!this.isEmpty()){
             No primeiroNo = refNoEntradaFila;
             while (true){
@@ -23,11 +24,11 @@ public class Fila {
                     break;
                 }
             }
-            return primeiroNo;
+            return(T) primeiroNo.getObject();
         }
         return null;
     }
-    public No dequeue(){
+    public T dequeue(){
         if(!this.isEmpty()){
             No primeiroNo = refNoEntradaFila;
             No noauxiliar = refNoEntradaFila;
@@ -40,7 +41,7 @@ public class Fila {
                     break;
                 }
             }
-            return primeiroNo;
+            return(T) primeiroNo.getObject();
         }
         return null;
     }
